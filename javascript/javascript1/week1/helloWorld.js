51bd638de17d9d52ec2baf973e33e28aad0b068a
Rami Khatib <rami@khateb.info>
2021-12-10 11:32:07 +0100

    My homework JavaScript week1

--- javascript/javascript1/week1/helloWorld.js
+++ javascript/javascript1/week1/helloWorld.js
@@ -1,1 +1,22 @@
 console.log("HelloWorld");
+// Age-ify (A future age calculator)
+
+let yearOfBirth = 1985;
+let yearFuture = 2080;
+let age = yearFuture - yearOfBirth;
+
+
+console.log("You will be " + age + " years old in " + yearFuture)
+
+// Goodboy-Oldboy (A dog age calculator)
+
+let dogYearOfBirth = 1985;
+let dogYearFuture = 2060;
+let dogYear;
+let shouldShowResultInDogYears = true;
+let resultInHumanYears;
+if (dogYear) {
+    shouldShowResultInDogYears = (dogYearFuture - dogYearOfBirth) * 7;
+    console.log(
+        "Your dog will be " + shouldShowResultInDogYears + " dog years old in 2027"
+    );
